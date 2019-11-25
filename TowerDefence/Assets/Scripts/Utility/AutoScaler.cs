17d7c28a51a9fdeaf39c876dbf192c53fe79b2c1
Chad Bissonnette [Student] <chad.bissonnette@student.sl.on.ca>
2019-11-25 15:51:44 -0500

    Enemy Scripts and Wave Manager

---
+++ TowerDefence/Assets/Scripts/Utility/AutoScaler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScaler : MonoBehaviour
+{
+    public float scaleSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += (new Vector3(scaleSpeed, scaleSpeed, 
+        scaleSpeed) * Time.deltaTime);
+    }
+
+    public static Quaternion SmoothlyLook(Transform fromTransform, 
+    Vector3 toVector3)
+    {
+        if (fromTransform.position == toVector3)
+        {
+            return fromTransform.localRotation;
+        }
+
+        Quaternion currentRotation = fromTransform.localRotation;
+        Quaternion targetRotation = Quaternion.LookRotation(toVector3 - 
+        fromTransform.position);
+
+        return Quaternion.Slerp(currentRotation, targetRotation, 
+        Time.deltaTime * 10f);
+    }
+}
