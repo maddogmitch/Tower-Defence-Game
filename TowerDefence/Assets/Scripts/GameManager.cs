1978f5e0527b7f5f4dd49a9215e154f1acefa3ed
snick harrison <harrisonnick16@gmai.com>
2019-12-01 13:43:36 -0500

    got rid of test scripts

--- TowerDefence/Assets/Scripts/GameManager.cs
+++ TowerDefence/Assets/Scripts/GameManager.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-    //
+    // static variable for global access
     public static GameManager Instance;
-    //
+    //amount of gold player has
     public int gold;
+    //num of wave currently being spawned
+    public int waveNumber;
+    //
+    public int escapedEnemies;
+    //
+    public int maxAllowedEscapedEnemies = 5;
+    //
+    public bool enemySpawningOver;
+    //
+    public AudioClip gameWinSound;
+    public AudioClip gameLoseSound;
+    //
+    private bool gameOver;
     // Start is called before the first frame update
     void Start()
     {
