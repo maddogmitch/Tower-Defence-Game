d2acad1ee4746d4eeb0074f9044620a69c1c30de
Chad Bissonnette [Student] <chad.bissonnette@student.sl.on.ca>
2019-12-02 14:57:20 -0500

    Tower info- In Progress

--- TowerDefence/Assets/Scripts/Game/UI/TowerInfoWindow.cs
+++ TowerDefence/Assets/Scripts/Game/UI/TowerInfoWindow.cs
@@ -20,6 +20,36 @@ public class TowerInfoWindow : MonoBehaviour
         btnUpgrade = txtUpgradeCost.transform.parent.gameObject;
     }
 
+    void OnEnable()
+    {
+        UpdateInfo();
+    }
+
+    private void UpdateInfo()
+    {
+        upgradePrice = Mathf.CeilToInt(TowerManager.Instance.GetTowerPrice(tower.type) * 1.5f * tower.towerLevel);
+        txtInfo.text = tower.type + " Tower Lv " + tower.towerLevel;
+
+        if (tower.towerLevel < 3)
+        {
+            btnUpgrade.SetActive(true);
+            txtUpgradeCost.text = "Upgrade\n" + upgradePrice + " Gold";
+        }
+        else
+        {
+            btnUpgrade.SetActive(false);
+        }
+    }
+
+    public void UpgradeTower()
+    {
+        if (GameManager.Instance.gold >= upgradePrice)
+        {
+            GameManager.Instance.gold -= upgradePrice; tower.LevelUp();
+            gameObject.SetActive(false);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
