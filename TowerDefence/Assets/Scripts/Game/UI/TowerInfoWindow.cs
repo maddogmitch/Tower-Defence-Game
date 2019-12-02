c2392c1cd317f4fa1de20187462e6e3dd8adaf1c
Chad Bissonnette [Student] <chad.bissonnette@student.sl.on.ca>
2019-12-02 14:53:20 -0500

    Tower info- In Progress

---
+++ TowerDefence/Assets/Scripts/Game/UI/TowerInfoWindow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerInfoWindow : MonoBehaviour
+{
+    //Reference to the tower that can be upgraded
+    public Tower tower;
+    // Text Components for the button
+    public Text txtInfo;
+    public Text txtUpgradeCost;
+    //The tpwer upgrade cost
+    private int upgradePrice;
+    //Reference to the Upgrade button
+    private GameObject btnUpgrade;
+
+    void Awake()
+    {
+        btnUpgrade = txtUpgradeCost.transform.parent.gameObject;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
