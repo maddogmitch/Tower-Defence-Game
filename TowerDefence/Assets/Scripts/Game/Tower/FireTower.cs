a9208da3290d915d152ad0bddf54b6bea9f97fb4
Nicholas Harrison [Student] <nicholas.harrison@student.sl.on.ca>
2019-12-06 12:20:15 -0500

    add fire tower script

---
+++ TowerDefence/Assets/Scripts/Game/Tower/FireTower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTower : Tower
+{
+    //refernce to the fire particles
+    public GameObject fireParticlesPrefab;
+
+    //over ride is used to exspand on the attck in the tower class
+    protected override void AttackEnemy()
+    {
+        //call the tower class attack enemy
+        base.AttackEnemy();
+        //spawn new fire particles
+        GameObject particles = (GameObject)Instantiate(fireParticlesPrefab,transform.position + new Vector3(0, .5f),fireParticlesPrefab.transform.rotation);
+        // Scale fire particle radius with the aggro radius aka change the range
+        particles.transform.localScale *= aggroRadius / 10f;
+
+        //damage all enemies in the attack radius 
+        foreach (Enemy enemy in EnemyManager.Instance.GetEnemiesInRange(transform.position, aggroRadius))
+        {
+            enemy.TakeDamage(attackPower);
+        }
+    }
+}
